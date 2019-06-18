ee7664dfa178015681cc24b35198fccbe1f33943
frederickpukay <frederickpukay@gmail.com>
2019-06-18 16:43:09 -0700

    feat: Handle database connections

--- tacocat.py
+++ tacocat.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, g
+
+import models
 
 DEBUG = True
 HOST = 'localhost'
 PORT = 8000
 
 app = Flask(__name__)
+
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    g.db = models.DATABASE
+    g.db.connect()
+    g.user = current_user
+
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    g.db.close()
+    return response
 
 
 @app.route('/')
