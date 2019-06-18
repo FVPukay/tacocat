138adb13da7d47317cf2ef7aaf4127348b4a5d4c
frederickpukay <frederickpukay@gmail.com>
2019-06-18 15:23:22 -0700

    feat: Add User model

---
+++ models.py
@@ -0,0 +1,23 @@
+from flask_bcrypt import bcrypt
+from flask_login import UserMixin
+from peewee import *
+
+DATABASE = SqliteDatabase('tacocat.db')
+
+
+class User(UserMixin, Model):
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+
+    class Meta:
+        database = DATABASE
+
+    @classmethod
+    def create_user(cls, email, password):
+        try:
+            cls.create(
+                email=email,
+                password=generate_password_hash(password)
+            )
+        except IntegrityError:
+            raise ValueError("User already exists")
