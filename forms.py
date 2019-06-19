280030bc768efc83c24ace34e4c9ac43c07d44eb
frederickpukay <frederickpukay@gmail.com>
2019-06-19 16:18:05 -0700

    feat: Add login view

--- forms.py
+++ forms.py
@@ -31,3 +31,8 @@ class RegisterForm(FlaskForm):
         'Confirm Password',
         validators=[DataRequired()]
     )
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
--- tacocat.py
+++ tacocat.py
@@ -1,5 +1,6 @@
 from flask import Flask, g, render_template, url_for, flash, redirect
-from flask_login import LoginManager, current_user
+from flask_bcrypt import check_password_hash
+from flask_login import LoginManager, current_user, login_user
 
 import forms
 import models
@@ -56,6 +57,24 @@ def register():
         )
         return redirect(url_for('index'))
     return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        try:
+            user = models.User.get(models.User.email == form.email.data)
+        except models.DoesNotExist:
+            flash("You're email or password doesn't match!", "error")
+        else:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user)
+                flash("You've been logged in!", "success")
+                return redirect(url_for('index'))
+            else:
+                flash("You're email or password doesn't match!", "error")
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
