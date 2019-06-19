1f67ca0821c1d18fd7055317304ee511054d465e
frederickpukay <frederickpukay@gmail.com>
2019-06-19 08:31:59 -0700

    fix: Update before request

--- tacocat.py
+++ tacocat.py
@@ -28,7 +28,7 @@ def load_user(userid):
 def before_request():
     """Connect to the database before each request."""
     g.db = models.DATABASE
-    g.db.connect()
+    g.db.get_conn()
     g.user = current_user
 
 
