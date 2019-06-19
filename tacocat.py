1c2fcace4a27c129d582120b1548b46c12d3a6c4
frederickpukay <frederickpukay@gmail.com>
2019-06-18 17:11:05 -0700

    feat: Load users with LoginManager

--- tacocat.py
+++ tacocat.py
@@ -1,4 +1,5 @@
 from flask import Flask, g
+from flask_login import LoginManager
 
 import models
 
@@ -7,6 +8,19 @@ HOST = 'localhost'
 PORT = 8000
 
 app = Flask(__name__)
+app.secret_key = 'vlfTkFDU9UpQsyvxD0C8Pg'
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
 
 
 @app.before_request
